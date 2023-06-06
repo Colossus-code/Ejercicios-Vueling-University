01e9ebaf90646867977b8c208a2b139eb852c4ad
Colossus-Code <sergio35garcia25@gmail.com>
2023-06-07 00:29:48 +0200

    V.2.01 BankAccountMultiUserDDD, genero base de datos, añado relaciones y aplico DDD, falta por desarrollar.

---
+++ Unidad_4/Ejercicio_BankAccountDDD/1 - Presentation/Presentation/Helpers/ReflectionRegistrator.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Helpers
+{
+    public class ReflectionRegistrator
+    {
+
+        public IContainer RegisterDependencies()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            RegisterCustomDependencies(containerBuilder);
+            var container = containerBuilder.Build();
+            return container;
+        }
+
+        private void RegisterCustomDependencies(ContainerBuilder containerBuilder)
+        {
+            
+        }
+    }
+}
---
+++ Unidad_4/Ejercicio_BankAccountDDD/4 - Repository/Repository/BankAccountRepository.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure.Entities;
+using Repository.DataBaseModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class BankAccountRepository
+    {
+        public List<BankAccount> GetBankAccounts() // METODO QUE EMPLEAREMOS PARA EL LOGIN INICIAL INTRODUCIENDO PIN Y NUMERO DE CUENTA
+        {
+            BankAccountsEntities dataBaseConnection = new BankAccountsEntities();
+
+            List<Accounts> bank = dataBaseConnection.Accounts.ToList();
+
+            List<BankAccount> bankAccounts = new List<BankAccount>(); // TODO sgarciam 0706 añadir propiedades a bankAccount para poder recorrer en un foreach
+
+            return null; 
+
+        }
+    }
+}
