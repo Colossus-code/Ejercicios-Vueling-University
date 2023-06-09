734af18b0f4f3c87e532af5789aab0d978f0ad2d
Colossus-Code <sergio35garcia25@gmail.com>
2023-06-09 18:56:14 +0200

    V.2.03 Genero test de la clase repositorio. Aplico y funcionan OK 13/13. Faltan test de los service aplicando mocking.

--- Unidad_4/Ejercicio_BankAccountDDD/4 - Repository/Repository/BankAccountRepository.cs
+++ Unidad_4/Ejercicio_BankAccountDDD/4 - Repository/Repository/BankAccountRepository.cs
@@ -38,201 +38,210 @@ namespace Repository
         }
         public bool IncomeOutcomeMoney(BankAccount bankAccountDomainEntity, decimal amount)
         {
-            
-            Accounts bankAccountDataEntity = _dbConnection.Accounts.FirstOrDefault(e=> e.Id == bankAccountDomainEntity.Id);
-            
-            if(bankAccountDataEntity != null)
+
+            try
             {
-                bankAccountDataEntity.AccountId = bankAccountDomainEntity.AccountId;
-                bankAccountDataEntity.AccountPin = bankAccountDomainEntity.AccountPin.ToString();
-                bankAccountDataEntity.Money = bankAccountDomainEntity.Money;
-                bankAccountDataEntity.Movements = new List<Movements>();
+                Accounts bankAccountDataEntity = null;
+
+
+                bankAccountDataEntity = _dbConnection.Accounts.FirstOrDefault(e => e.Id == bankAccountDomainEntity.Id);
+
+                if (bankAccountDataEntity != null)
+                {
+                    bankAccountDataEntity.AccountId = bankAccountDomainEntity.AccountId;
+                    bankAccountDataEntity.AccountPin = bankAccountDomainEntity.AccountPin.ToString();
+                    bankAccountDataEntity.Money = bankAccountDomainEntity.Money;
+                    bankAccountDataEntity.Movements = new List<Movements>();
+
+                }
+
+                List<Movements> movements = _dbConnection.Movements.ToList().Where(e => e.AccountId == bankAccountDomainEntity.Id).ToList();
+
+                if (movements != null)
+                {
+                    if (amount > 0)
+                    {
+                        movements.Add(new Movements()
+                        {
+                            Amount = amount,
+                            Date = DateTime.Now,
+                            AccountId = bankAccountDomainEntity.Id,
+                        });
+                    }
+                    else
+                    {
+                        movements.Add(new Movements()
+                        {
+                            Amount = amount,
+                            Date = DateTime.Now,
+                            AccountId = bankAccountDomainEntity.Id,
+                        });
+                    }
+                    bankAccountDataEntity.Movements = movements;
+
+                    _dbConnection.SaveChanges();
+
+                    return true;
+                }
+
+                return false;
 
             }
-           
-            List<Movements> movements = _dbConnection.Movements.ToList().Where(e => e.AccountId == bankAccountDomainEntity.Id).ToList();
-
-            if(movements != null)
+            catch (Exception) { return false; }
+        }
+        public BankAccount GetBankAccountByAccountNumber(string bankAccountNumber)
             {
-                if (amount > 0)
+
+                Accounts dbAccount = _dbConnection.Accounts.FirstOrDefault(e => e.AccountId.Equals(bankAccountNumber));
+                BankAccount domainAccount = null;
+
+                if (dbAccount != null)
                 {
-                    movements.Add(new Movements()
+                    domainAccount = new BankAccount
                     {
-                        Amount = amount,
-                        Date = DateTime.Now,
-                        AccountId = bankAccountDomainEntity.Id,
-                    });
+                        Id = dbAccount.Id,
+                        AccountId = dbAccount.AccountId,
+                        AccountPin = Convert.ToInt32(dbAccount.AccountPin),
+                        Money = dbAccount.Money,
+                        MovementId = new List<int>()
+                    };
                 }
                 else
                 {
-                    movements.Add(new Movements()
+                    return null;
+                }
+
+                List<Movements> moves = _dbConnection.Movements.Where(e => e.AccountId == domainAccount.Id).ToList();
+
+                if (moves != null)
+                {
+                    foreach (Movements movement in moves)
                     {
-                        Amount = amount,
-                        Date = DateTime.Now,
-                        AccountId = bankAccountDomainEntity.Id,
+                        domainAccount.MovementId.Add(movement.Id);
+                    }
+                }
+
+                return domainAccount;
+            }
+
+            public bool ChangePinAccount(string bankAccountId, string newPin)
+            {
+                Accounts account = _dbConnection.Accounts.FirstOrDefault(e => e.AccountId == bankAccountId);
+
+                if (account != null)
+                {
+                    account.AccountPin = newPin;
+
+                    _dbConnection.SaveChanges();
+
+                    return true;
+                }
+
+                return false;
+            }
+
+            public bool CreateBankAccount(BankAccount bankAccount)
+            {
+                Accounts accountDataEntity = null;
+                try
+                {
+                    accountDataEntity = new Accounts
+                    {
+                        AccountId = bankAccount.AccountId,
+                        AccountPin = bankAccount.AccountPin.ToString(),
+                        Money = bankAccount.Money,
+                    };
+                    _dbConnection.Accounts.Add(accountDataEntity);
+                    _dbConnection.SaveChanges();
+
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+
+                if (accountDataEntity.Money != 0)
+                {
+                    Movements initialMovementDataEntity = new Movements
+                    {
+                        AccountId = accountDataEntity.Id,
+                        Amount = accountDataEntity.Money,
+                        Date = DateTime.UtcNow,
+
+                    };
+                    _dbConnection.Movements.Add(initialMovementDataEntity);
+                    _dbConnection.SaveChanges();
+                }
+
+                if (accountDataEntity != null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public bool DeleteBankAccount(int idAccount)
+            {
+                Accounts dataDomainBankAccountDelete = _dbConnection.Accounts.Find(idAccount);
+                List<Movements> dataDomainMovementsDelete = null;
+
+                if (dataDomainBankAccountDelete != null && dataDomainBankAccountDelete.Movements.Count > 0)
+                {
+                    dataDomainMovementsDelete = _dbConnection.Movements.Where(e => e.AccountId == idAccount).ToList();
+                }
+
+                if (dataDomainMovementsDelete != null)
+                {
+                    foreach (Movements move in dataDomainMovementsDelete)
+                    {
+                        _dbConnection.Movements.Remove(move);
+                    }
+
+                    _dbConnection.SaveChanges();
+                }
+
+                if (dataDomainBankAccountDelete != null)
+                {
+                    _dbConnection.Accounts.Remove(dataDomainBankAccountDelete);
+                    _dbConnection.SaveChanges();
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public List<BankAccount> GetAllAccounts()
+            {
+                List<Accounts> dataEntityAccounts = _dbConnection.Accounts.ToList();
+
+                List<BankAccount> bankAccounts = new List<BankAccount>();
+
+                if (dataEntityAccounts == null)
+                {
+                    return bankAccounts;
+                }
+
+                foreach (Accounts account in dataEntityAccounts)
+                {
+                    bankAccounts.Add(new BankAccount
+                    {
+                        Id = account.Id,
+                        AccountId = account.AccountId,
+                        AccountPin = Convert.ToInt32(account.AccountPin),
+                        Money = account.Money
+
                     });
-                }
-            bankAccountDataEntity.Movements = movements;
 
-                _dbConnection.SaveChanges();
-
-                return true; 
-            }
-
-            return false;
-            //TODO 0806 añadir movimientos
-        }
-        public BankAccount GetBankAccountByAccountNumber(string bankAccountNumber)
-        {
-
-            Accounts dbAccount = _dbConnection.Accounts.FirstOrDefault(e => e.AccountId.Equals(bankAccountNumber));
-            BankAccount domainAccount = null;
-
-            if (dbAccount != null)
-            {
-                domainAccount = new BankAccount
-                {
-                    Id = dbAccount.Id,
-                    AccountId = dbAccount.AccountId,
-                    AccountPin = Convert.ToInt32(dbAccount.AccountPin),
-                    Money = dbAccount.Money,
-                    MovementId = new List<int>()
-                };
-            }
-            else
-            {
-                return null;
-            }
-
-            List<Movements> moves = _dbConnection.Movements.Where(e => e.AccountId == domainAccount.Id).ToList();
-
-            if (moves != null)
-            {
-                foreach (Movements movement in moves)
-                {
-                    domainAccount.MovementId.Add(movement.Id);
-                }
-            }
-
-            return domainAccount;
-        }
-
-        public bool ChangePinAccount(string bankAccountId, string newPin)
-        {
-            Accounts account = _dbConnection.Accounts.FirstOrDefault(e => e.AccountId ==  bankAccountId);
-
-            if (account != null)
-            {
-                account.AccountPin = newPin;
-
-                _dbConnection.SaveChanges();
-
-                return true;
-            }
-
-            return false;
-        }
-
-        public bool CreateBankAccount(BankAccount bankAccount)
-        {
-            Accounts accountDataEntity = new Accounts
-            {
-                AccountId = bankAccount.AccountId,
-                AccountPin = bankAccount.AccountPin.ToString(),
-                Money = bankAccount.Money,
-            };
-            try
-            {
-                _dbConnection.Accounts.Add(accountDataEntity);
-                _dbConnection.SaveChanges();
-            
-            }catch (Exception)
-            {
-                return false; 
-            }
-
-            if(accountDataEntity.Money != 0)
-            {
-                Movements initialMovementDataEntity = new Movements
-                {
-                    AccountId = accountDataEntity.Id,
-                    Amount = accountDataEntity.Money,
-                    Date = DateTime.UtcNow,
-
-                };
-                _dbConnection.Movements.Add(initialMovementDataEntity);
-                _dbConnection.SaveChanges();
-            }
-
-            if(accountDataEntity != null)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public bool DeleteBankAccount(int idAccount)
-        {
-            Accounts dataDomainBankAccountDelete = _dbConnection.Accounts.Find(idAccount);
-            List<Movements> dataDomainMovementsDelete = null; 
-
-            if (dataDomainBankAccountDelete != null && dataDomainBankAccountDelete.Movements.Count > 0)
-            {
-                dataDomainMovementsDelete = _dbConnection.Movements.Where(e => e.AccountId == idAccount).ToList();
-            }
-
-            if(dataDomainMovementsDelete != null)
-            {
-                foreach (Movements move in dataDomainMovementsDelete)
-                {
-                    _dbConnection.Movements.Remove(move);
                 }
 
-                _dbConnection.SaveChanges();
-            }
-
-            if(dataDomainBankAccountDelete != null)
-            {
-                _dbConnection.Accounts.Remove(dataDomainBankAccountDelete);
-                _dbConnection.SaveChanges();
-
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public List<BankAccount> GetAllAccounts()
-        {
-            List <Accounts> dataEntityAccounts = _dbConnection.Accounts.ToList();
-
-            List <BankAccount> bankAccounts = new List<BankAccount>();
-
-            if(dataEntityAccounts == null)
-            {
                 return bankAccounts;
             }
-
-            foreach (Accounts account in dataEntityAccounts)
-            {
-                bankAccounts.Add(new BankAccount
-                {
-                    Id = account.Id,
-                    AccountId = account.AccountId,
-                    AccountPin = Convert.ToInt32(account.AccountPin),
-                    Money = account.Money
-
-                });
-
-            }
-
-            return bankAccounts;
         }
     }
-}
 
---
+++ Unidad_4/Ejercicio_BankAccountDDD/5 - Utilities/Test/BankAccountTest/BankAccountRepositorySuitTest.cs
@@ -0,0 +1,219 @@
+﻿using Infrastructure.Entities;
+using Infrastructure.IRepository;
+using Moq;
+using Repository;
+using Repository.DataBaseModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankAccountTest
+{
+    public class BankAccountRepositorySuitTest
+    {
+        //First of all, creating a object to test CRUD -> 
+        private BankAccount _testBankAccountCreate;
+        private BankAccount _testBankAccountDelete;
+
+        private readonly BankAccountRepository _bankAccountRepo;
+
+        //private readonly Mock<IRepositoryBankAccount> _repoBankAccount = new Mock<IRepositoryBankAccount>();
+
+        public BankAccountRepositorySuitTest()
+        {
+            _bankAccountRepo = new BankAccountRepository();
+        }
+        private void InitializeObject()
+        {
+            _testBankAccountCreate = new BankAccount()
+            {
+                AccountId = "TSTBANK02",
+                AccountPin = 1234,
+                Money = 400
+            };
+
+            _testBankAccountDelete = new BankAccount()
+            {
+                AccountId = "TSTBANKDEL01",
+                AccountPin = 1234,
+                Money = 400
+            };
+        }
+
+        #region Create region
+        // CREATE BANK ACCOUNT CASE 
+        [Fact]
+        public void InputCorrectData_CreateBankAccount_AssertTrue()
+        {
+            InitializeObject();
+
+            Assert.True(_bankAccountRepo.CreateBankAccount(_testBankAccountCreate));
+
+        }
+
+        [Fact]
+        public void InputWrongData_CreateBankAccount_AssertFalse()
+        {
+            InitializeObject(); // DUPLICATED ACCOUNT
+
+            Assert.False(_bankAccountRepo.CreateBankAccount(_testBankAccountCreate));
+
+
+        }
+        [Fact]
+        public void InputNullData_CreateBankAccount_AssertFalse()
+        {
+
+            Assert.False(_bankAccountRepo.CreateBankAccount(null)); // NULL OBJECT 
+
+        }
+
+        #endregion
+
+        #region Read region
+        [Fact]
+        public void InputCorrectData_FindAccount_AssertNotNull()
+        {
+            InitializeObject();
+
+            Assert.NotNull(_bankAccountRepo.GetBankAccountByAccountNumber("TSTBANK02"));
+
+        }
+
+        [Fact]
+        public void InputWrongData_FindAccount_AssertNotNull()
+        {
+
+            Assert.Null(_bankAccountRepo.GetBankAccountByAccountNumber("ES-1999"));
+
+
+        }
+        #endregion
+
+        #region Update region
+        [Fact]
+        public void InputCorrectData_UpdateMoneyPositiveAmount_AssertTrue()
+        {
+            InitializeObject();
+
+            BankAccountsEntities1 dataEntityAcc = new BankAccountsEntities1();
+
+            _testBankAccountCreate.Id = dataEntityAcc.Accounts.FirstOrDefault(e => e.AccountId.Equals(_testBankAccountCreate.AccountId)).Id;
+
+            Assert.True(_bankAccountRepo.IncomeOutcomeMoney(_testBankAccountCreate, 600)); // POSITIVE 
+
+        }
+
+        [Fact]
+        public void InputCorrectData_UpdateMoneyNegativeAmount_AssertTrue()
+        {
+            InitializeObject();
+
+            BankAccountsEntities1 dataEntityAcc = new BankAccountsEntities1();
+
+            _testBankAccountCreate.Id = dataEntityAcc.Accounts.FirstOrDefault(e => e.AccountId.Equals(_testBankAccountCreate.AccountId)).Id;
+
+            Assert.True(_bankAccountRepo.IncomeOutcomeMoney(_testBankAccountCreate, -400)); // NEGATIVE 
+        }
+
+
+        [Fact]
+        public void InputWrongData_UpdateMoneyPositiveMoney_AssertFalse()
+        {
+            BankAccount wrongBankAccount = new BankAccount
+            {
+                Id = -5,
+                AccountId = "ES002",
+                AccountPin = 1234,
+                Money = 0,
+            };
+
+            Assert.False(_bankAccountRepo.IncomeOutcomeMoney(wrongBankAccount,1000));
+
+        }
+
+        [Fact]
+        public void InputWrongData_UpdateMoneyNegativeMoney_AssertFalse()
+        {
+            BankAccount wrongBankAccount = new BankAccount
+            {
+                Id = -5,
+                AccountId = "ES002",
+                AccountPin = 1234,
+                Money = 0,
+            };
+
+            Assert.False(_bankAccountRepo.IncomeOutcomeMoney(wrongBankAccount, -1000));
+
+        }
+        //[Fact]
+        //public void InputWrongData_UpdateMoneyNegativeMoney_AssertString()
+        //{
+        //    BankAccount wrongBankAccount = new BankAccount
+        //    {
+        //        Id = -5,
+        //        AccountId = "ES002",
+        //        AccountPin = 1234,
+        //        Money = 0,
+        //    };
+
+        //    Assert.False(_bankAccountRepo.IncomeOutcomeMoney(wrongBankAccount, -1000));
+
+        //}
+
+        [Fact]
+        public void InputCorrectData_UpdatePinAccount_AssertTrue()
+        {
+            InitializeObject();
+
+            Assert.True(_bankAccountRepo.ChangePinAccount(_testBankAccountCreate.AccountId, "1234"));
+
+        }
+
+        [Fact]
+        public void InputWrongData_UpdatePinAccount_AssertFalse()
+        {
+
+            Assert.False(_bankAccountRepo.ChangePinAccount("ES-1999", "12345"));
+
+
+        }
+
+        #endregion
+
+        #region Delete region
+        [Fact]
+        public void InputCorrectData_DeleteBankAccount_AssertTrue()
+        {
+            InitializeObject();
+            
+            BankAccountsEntities1 _dbConn = new BankAccountsEntities1();
+
+            _dbConn.Accounts.Add(new Accounts
+            {
+                AccountId = _testBankAccountDelete.AccountId,
+                AccountPin = _testBankAccountDelete.AccountPin.ToString(),
+                Money = _testBankAccountDelete.Money,
+
+            });
+
+            _dbConn.SaveChanges();
+
+                Assert.True(_bankAccountRepo.DeleteBankAccount(_dbConn.Accounts.FirstOrDefault(e => e.AccountId.Equals(_testBankAccountDelete.AccountId)).Id));
+
+        }
+
+        [Fact]
+        public void InputWrongData_DeleteBankAccount_AssertFalse()
+        {
+
+            Assert.False(_bankAccountRepo.DeleteBankAccount(-1999));
+
+
+        }
+        #endregion
+    }
+}
